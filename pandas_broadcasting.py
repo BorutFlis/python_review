ad162a6bad4de0ca3c9dcc301080589d18b2e34c
Flis <borut.flis@fri1.uni-lj.si>
2023-11-30 16:04:32 +0100

    new broadcasting task

--- pandas_broadcasting.py
+++ pandas_broadcasting.py
@@ -5,10 +5,11 @@ df = pd.read_csv("test_data/england-premier-league-matches-2017-to-2018-stats.cs
 last_row = df.iloc[-1]
 
 df_with_nans = df.loc[:, ["home_team_goal_count", "away_team_goal_count"]]
-df_with_nans.iloc[np.random.randint(0, len(df), 10), 1] = np.nan
-df_with_nans.iloc[np.random.randint(0, len(df), 10), 0] = np.nan
+df_with_nans.iloc[np.random.randint(0, len(df), 100), 1] = np.nan
+df_with_nans.iloc[np.random.randint(0, len(df), 100), 0] = np.nan
 
-# task 1: make dummies out of team_name and then broadcast the home_team_goal_count to the dummy matrix
+# task 1a: make dummies out of team_name and then broadcast the home_team_goal_count to the dummy matrix
+#      1b: get series with first non-na for each column
 # task 2: fill na values in df_with_nans with mean of home/away goal count do it for both axes
 # task 3: add last row to data-frame
 # task 4: multiply each column with a different value
