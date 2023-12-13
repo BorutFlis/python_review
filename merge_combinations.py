1d444ffb67c4e67bdcf95ee7480db3e8ff6769f7
Borut <borutflis1@gmail.com>
2023-12-13 08:22:31 +0100

    added merge

---
+++ merge_combinations.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv("test_data/germany-bundesliga-matches-2018-to-2019-stats.csv")
+
+# task 1: how many rows after merge on Game Week
+# task 2: drop duplicate initial rows
+# task 3: drop duplicate merge combinations
