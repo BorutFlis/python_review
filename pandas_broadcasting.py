388babdfc34bbad7b2be42663885c82300d50c42
borut.flis <borut.flis@fri.uni-lj.si>
2024-04-11 15:56:21 +0200

    multi index multiply

--- pandas_broadcasting.py
+++ pandas_broadcasting.py
@@ -8,10 +8,19 @@ df_with_nans = df.loc[:, ["home_team_goal_count", "away_team_goal_count"]]
 df_with_nans.iloc[np.random.randint(0, len(df), 100), 1] = np.nan
 df_with_nans.iloc[np.random.randint(0, len(df), 100), 0] = np.nan
 
+df1 = df[["home_team_goal_count", "home_team_shots"]]
+df2 = df[["away_team_goal_count", "away_team_shots"]]
+df_multi = pd.concat([
+    df1.set_axis(pd.MultiIndex.from_tuples([("h", c) for c in df1.columns]), axis=1),
+    df2.set_axis(pd.MultiIndex.from_tuples([("a", c) for c in df2.columns]), axis=1)
+], axis=1)
+multipliers = {"h":1, "a":22}
+
 # task 1a: make dummies out of team_name and then broadcast the home_team_goal_count to the dummy matrix
 #      1b: try to do 1a with pivot
 #      1c: get series with first non-na for each column
 #      1d: reverse get dummies operation
 # task 2: fill na values in df_with_nans with mean of home/away goal count do it for both axes
 # task 3: add last row to data-frame
-# task 4: multiply each column with a different value
+# task 4a: multiply each column with a different value
+# task 4b: multiply df_multi based on top-level and multipliers dict
